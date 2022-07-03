835f5a5dde86572fa45f0acbc749aa29a5b9e314
susana <sulopezfeijoo@gmail.com>
2022-07-03 18:08:53 +0200

    mongo

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,17 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { MongooseModule } from "@nestjs/mongoose";
 import { CoreModule } from "src/core/core.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtStrategyService } from './jwt-strategy/jwt-strategy.service';
+import { User, UserSchema } from "./models/user.entity";
 
 @Module({
-  imports: [CoreModule, JwtModule.register({})],
+  imports: [
+    MongooseModule.forFeature([{name: User.name, schema: UserSchema}]),
+    CoreModule, 
+    JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategyService],
 })
--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,12 +1,22 @@
 import { MiddlewareConsumer, Module, NestModule, ValidationPipe } from "@nestjs/common";
 import { APP_GUARD, APP_PIPE } from "@nestjs/core";
+import { MongooseModule } from "@nestjs/mongoose";
 import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 import helmet from "helmet";
 import { MonitorMiddleware } from "./middlewares/monitor.middleware";
 import { UtilsService } from "./utils/utils.service";
 
+const mongoUser = "nest_user";
+const mongoPass = "nest_password";
+const mongoHost = "localhost:27017";
+const mongoDB = "nest";
+const mongoUri = `mongodb://${mongoUser}:${mongoPass}@${mongoHost}/${mongoDB}?authSource=admin`;
+
 @Module({
-  imports: [ThrottlerModule.forRoot({ ttl: 60, limit: 10 })],
+  imports: [
+    ThrottlerModule.forRoot({ ttl: 60, limit: 10 }),
+    MongooseModule.forRoot(mongoUri)
+  ],
   providers: [
     UtilsService,
     {
